c7b9219a46b0d0aeeed1e368d32b4090c85a0ca4
gustavao <elg.gspindola@gmail.com>
2020-07-19 14:56:24 -0500

    personalData

--- src/app/models/personal-data.module.ts
+++ src/app/models/personal-data.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+export class PersonalData{
+
+  constructor(
+      public email:string,
+      public password:string,
+      //opts
+      public paso?:string,
+      public rePassword?:string
+      ){}
 
 
-
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
-})
-export class PersonalDataModule { }
+}
