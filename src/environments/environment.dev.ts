44f5f41d9ce683ff9f3a2da7cf40b451eb3ecf69
gustavao <elg.gspindola@gmail.com>
2020-07-22 14:14:26 -0500

    environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,14 +1,17 @@
 export const environment = {
   production: true,
-  apis_url : 'https://apis.bancodelbienestar.com.mx',
-  mifos_url : 'https://backoffice.bancodelbienestar.com.mx',
+  apis_url : 'https://apis-pre.bancodelbienestar.com.mx',
+  mifos_url : 'https://backoffice-pre.bancodelbienestar.com.mx',
   headers_mifos : {
     'Fineract-Platform-TenantId': 'default',
     'Content-Type': 'application/json', 'Content-Encoding': 'Gzip'
   },
   headers_apis : {
-    'X-Gravitee-Api-Key': 'apiprod',
+    'X-Gravitee-Api-Key': 'debe ser una llava de desarrolo',
     'Content-Type': 'application/json'
   }
+
 };
+
+
 
