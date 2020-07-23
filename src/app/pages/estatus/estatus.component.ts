c4c85957475009efc4c7acc9fbe0978b4f5d2717
karen <ana.avila@infotec.mx>
2020-07-23 13:08:19 -0500

    Mensaje de confirmacion

---
+++ src/app/pages/estatus/estatus.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert';
+
+@Component({
+  selector: 'app-estatus',
+  templateUrl: './estatus.component.html',
+  styleUrls: ['./estatus.component.css']
+})
+export class EstatusComponent implements OnInit {
+
+  constructor() { }
+
+ 
+  ngOnInit() {
+}
+
+
+}
