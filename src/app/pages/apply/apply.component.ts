6bef46853807095b10459a906bb26897f30e414c
Alberto Hernandez <jose.hernandez@fintecheando.mx>
2020-07-26 16:08:28 -0500

    Actualizacion para uso de SHA256 en contraseña

--- src/app/pages/apply/apply.component.ts
+++ src/app/pages/apply/apply.component.ts
@@ -9,7 +9,6 @@ import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../../services/service.index';
 import { User } from '../../models/user.model';
-import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-apply',
@@ -17,11 +16,11 @@ import * as CryptoJS from 'crypto-js';
   styleUrls: ['./apply.component.css']
 })
 export class ApplyComponent implements OnInit {
-  instance 
+  instance
   popup;
   recaptcha: any[];
   //-----
-  form : FormGroup;
+  form: FormGroup;
   formval: FormGroup;
   dic = [
     "apañar",
@@ -32,24 +31,23 @@ export class ApplyComponent implements OnInit {
   //-------
   passType = "password";
   rePassType = "password";
-  icon:boolean=true;
-  reIcon:boolean=true;
-
+  icon: boolean = true;
+  reIcon: boolean = true;
 
   resolved(captchaResponse: any[]) {
     this.recaptcha = captchaResponse;
     // console.log(this.recaptcha);
   }
 
-  constructor(public userService:UserService, private router: Router) {
-    
+  constructor(public userService: UserService, private router: Router) {
+
   }
 
   ngOnInit() {
 
     var elems = document.querySelectorAll('.modal');
     this.instance = M.Modal.init(elems);
-    
+
     var select = document.querySelectorAll('select');
     var instances = M.FormSelect.init(select);
 
@@ -70,123 +68,116 @@ export class ApplyComponent implements OnInit {
       stepTitleNavigation: false,
       // Preloader used when step is waiting for feedback function. If not defined, Materializecss spinner-blue-only will be used.
       feedbackPreloader: '<div class="spinner-layer spinner-blue-only">...</div>'
-   })
+    })
 
-//----------------------
-
-
-   this.form = new FormGroup({  
-      email: new FormControl(null,[Validators.required, Validators.email]),
+    //----------------------
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$')]),
       rePassword: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$')])
-   }, { validators: this.equalPass('password','rePassword') });
+    }, { validators: this.equalPass('password', 'rePassword') });
 
-   //validators: this.pbaDict('password') 
-
-//------------------------   
-   this.formval = new FormGroup({
-    codigo: new FormControl(null,[Validators.required, Validators.minLength(3),  Validators.maxLength(3), Validators.pattern('[0-9]{3}')]),
-    token: new FormControl(null, [Validators.required, Validators.minLength(3),  Validators.maxLength(3), Validators.pattern('[0-9]{3}')])
- }
- );
+    //validators: this.pbaDict('password') 
+    //------------------------   
+    this.formval = new FormGroup({
+      codigo: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(3), Validators.pattern('[0-9]{3}')]),
+      token: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(3), Validators.pattern('[0-9]{3}')])
+    }
+    );
 
   }
 
   get f() { return this.form.controls; }
   get fo() { return this.formval.controls; }
-//------------
+  //------------
 
-equalPass(p1:string,p2:string){
+  equalPass(p1: string, p2: string) {
 
-  return ( group:FormGroup)=>{
-    let pass1 = group.controls[p1].value;
-    let pass2 = group.controls[p2].value;
-    if(pass1 === pass2){
-      return null;
+    return (group: FormGroup) => {
+      let pass1 = group.controls[p1].value;
+      let pass2 = group.controls[p2].value;
+      if (pass1 === pass2) {
+        return null;
+      }
+      return { isEqual: true };
     }
-    return{isEqual:true};
   }
-}
 
-pbaDict(p1:string){
+  pbaDict(p1: string) {
 
-  return ( group:FormGroup)=>{
-    let val1 = group.controls[p1].value;
-    
-    for(let i of this.dic){
-      //// console.log("compare",val1,"vs",i)
-    if(val1 === i){
-      return {isMatch:true};
+    return (group: FormGroup) => {
+      let val1 = group.controls[p1].value;
+
+      for (let i of this.dic) {
+        //// console.log("compare",val1,"vs",i)
+        if (val1 === i) {
+          return { isMatch: true };
+        }
+      }
     }
-    
 
   }
-
-  }
-  
-}
-//------------
+  //------------
 
   //-------------
   register() {
     let user = new User(this.form.value.email,
-      CryptoJS.AES.encrypt(this.form.value.password, this.form.value.email).toString(),"1",
-      CryptoJS.AES.encrypt(this.form.value.rePassword, this.form.value.email).toString());
+      this.userService.createHash(this.form.value.password), "1",
+      this.userService.createHash(this.form.value.rePassword));
 
-    if(this.form.valid){
+    if (this.form.valid) {
       /* // console.log("form esto envio", this.form.value); */
       //enviar datos a back
       /* this.userService.createUser(this.form.value) */
       // console.log("apply envia", user);
       this.userService.createUser(user)
-        .subscribe(res=>{
+        .subscribe(res => {
           // console.log("esto responde el servicio register",res); //revisar res.user p.ej y hacer un if(uid){openmodal}
         });
 
-        //this.userService.createUserL(user);
+      //this.userService.createUserL(user);
 
 
       this.instance[0].open(); //revisar donde quedara
-    }else{
+    } else {
       //algo esta mal revisa tus datos
       swal("¡Cuidado!", "Para poder continuar, completa correctamente todos los campos.", "error");
     }
 
   }
   //-------------
-  viewRePassword(){
-    if(this.reIcon){
+  viewRePassword() {
+    if (this.reIcon) {
       //// console.log("view repassword");
-      this.reIcon=false;
-      this.rePassType="text";
-    }else{
+      this.reIcon = false;
+      this.rePassType = "text";
+    } else {
       //// console.log("not view repassword");
-      this.reIcon=true;
-      this.rePassType="password";
+      this.reIcon = true;
+      this.rePassType = "password";
     }
   }
 
-  viewPassword(){
-    if(this.icon){
+  viewPassword() {
+    if (this.icon) {
       //// console.log("view password");
-      this.icon=false;
-      this.passType="text";
-    }else{
+      this.icon = false;
+      this.passType = "text";
+    } else {
       //// console.log("not view password");
-      this.icon=true;
-      this.passType="password";
+      this.icon = true;
+      this.passType = "password";
     }
   }
 
-  validacion(){
-    if(this.formval.valid){
+  validacion() {
+    if (this.formval.valid) {
       // console.log("formval", this.formval.value);
       this.router.navigate(["home"]);
       //enviar datos a back
       //this.popup[0].open();
-    } else{
+    } else {
       swal("¡Cuidado!", "Para poder continuar, completa correctamente todos los campos.", "error");
     }
   }
-
 }
