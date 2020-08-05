35c4a47322dfe3b80c7d4605115ce3bf9eef1236
Gustavo Espindola <gustavo@MacGus.local>
2020-08-05 16:22:38 -0500

    loan data

--- src/app/services/loan-data/loan-data.service.ts
+++ src/app/services/loan-data/loan-data.service.ts
@@ -14,14 +14,14 @@ export class LoanDataService {
 
   getLoanData() {
     /* DESARROLLO */
-    let loanid = '90246';   
+   /*  let loanid = '90246';   
     let url = environment.mifos_url + '/fineract-provider/api/v1/loans/' + loanid + '/?associations=all&exclude=guarantors,futureSchedule';
     let headers = environment.provisional_header;
-    return this.http.get<any>(url, { headers: headers });
+    return this.http.get<any>(url, { headers: headers }); */
 
 
     /* PREPROD */
-/* 
+
       //let loanid = localStorage.getItem('loanid');
       let loanid = '180106';
       let url = environment.apis_url + '/V1.0/fineract-protected/loans/' + loanid + '/?associations=all&exclude=guarantors,futureSchedule';
@@ -29,7 +29,7 @@ export class LoanDataService {
       let headers = environment.headers_apis;
       headers['X-Gravitee-Api-Key'] = api_keys['fineract'];
   
-      return this.http.get<any>(url, { headers: headers }); */
+      return this.http.get<any>(url, { headers: headers });
   }
 
 }
